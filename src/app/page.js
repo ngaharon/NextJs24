ac63f5e06972fa6ef36a56609c125fbaf87d2093
ngaharon <ngairaharon1@gmail.com>
2024-08-24 22:01:59 +0300

    Navigation with links and useRouter hooks

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
+'use client'
+
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+//useRoute -> next/navigation
 
 export default function Home() {
+
+  const router = useRouter()
+
+  console.log(router);
+
+  function handleNavigate() {
+    router.push('products')
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>hi</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+     <h1>Welcome to my NextJs24 Course Journey</h1>
+     <Link href={'/products'}>navigate to products page</Link>
+     <Link href={'/account'}>navigate to account page</Link>
+
+     <h2 className="font-bold mt-3 text-lg">Alternative way of navigating using useRoute</h2>
+     <button onClick={handleNavigate}>Navigate to products page using use router</button>
     </main>
   );
 }
