7dd37820fe9862717218778ee145f96de157b0e4
ngaharon <ngairaharon1@gmail.com>
2024-08-24 23:02:23 +0300

    redirect path navigation on serve side

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,3 +1,12 @@
+
+import {  redirect } from "next/navigation";
+
 export default function account() {
+
+    // assume that profile info is null
+    const userProfileInfo = null;
+
+    if(userProfileInfo === null) redirect('profile')
+
     return <h1>Account page</h1>
 }
---
+++ src/app/profile/page.js
@@ -0,0 +1,10 @@
+
+
+
+export default function Profile() {
+    return <div>
+        <h1>this a profile page</h1>
+    </div>
+
+    
+}
