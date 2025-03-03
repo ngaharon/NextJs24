50f60cc7d399b22de269d6d8cb7dfc13cccc2739
ngaharon <ngairaharon1@gmail.com>
2025-03-03 13:22:12 +0300

    homepage herosection

--- digital-news-app/src/sections/Hero.tsx
+++ digital-news-app/src/sections/Hero.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
+import { heroSlides } from '@/data/data'
+import './hero.css'
+
+// import AOS from 'aos';
+import AOS from 'aos';
+
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import HeroSlide from '@/components/HeroSlide';
 
 export default function Hero() {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: false,
+            mirror: false,
+        });
+    }
+    , []);
   return (
-    <div>
-      Hero
-    </div>
+    <section id="hero-slider" className='hero-slider'>
+        <div className="container-md" data-aos="fade-in">
+            <div className="row">
+                <div className="col-12">
+                    <Swiper
+                        slidesPerView={'auto'}
+                        speed={500}
+                        autoplay={{
+                            delay: 3000,
+                            disableOnInteraction: false,
+                        }}
+                        pagination={{
+                            el: '.swiper-pagination',
+                            clickable: true,
+                            type: 'bullets',
+                        }}
+                        navigation={{
+                            nextEl: '.custom swiper-button-next',
+                            prevEl: '.custom swiper-button-prev',
+                        }}
+                        modules={[Navigation, Pagination, Autoplay]}
+                        loop={true}
+                        className='sliderFeaturedPosts'
+                    >
+                        {
+                            heroSlides.map(slide=>(
+                                <SwiperSlide>
+                                    {/*include customized hero slide template */}
+                                    <HeroSlide slide={slide}/>
+                                </SwiperSlide>
+                            ))
+                        }
+
+                        <div className="custom-swiper-button-next">
+                            <span className="bi-chevron-right"></span>
+                        </div>
+                        <div className="custom-swiper-button-prev">
+                            <span className="bi-chevron-left"></span>
+                        </div>
+                        <div className="swiper-pagination">
+                            
+                        </div>
+                    </Swiper>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
